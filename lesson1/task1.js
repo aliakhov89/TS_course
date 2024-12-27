ec529f22cc13ed9197e27f42d89f1f724dcc1d26
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-12-27 14:49:37 +0200

    added comments with tasks description

--- lesson1/task1.js
+++ lesson1/task1.js
@@ -1,3 +1,9 @@
 "use strict";
-let admin
-let name;
+//Оголосіть дві змінні: admin та name.
+//Присвойте значення "Іван" змінній name.
+//Скопіюйте значення зі змінної name в admin.
+//Виведіть значення змінної admin, використовуючи функцію alert (яка повинна показати “Іван”).
+let admin, name1; //used name1 instead name because nema was already in use
+name1 = 'Іван';
+admin = name1;
+console.log(admin);
--- lesson1/task1.ts
+++ lesson1/task1.ts
@@ -1,4 +1,9 @@
-let admin, name1;
+//Оголосіть дві змінні: admin та name.
+//Присвойте значення "Іван" змінній name.
+//Скопіюйте значення зі змінної name в admin.
+//Виведіть значення змінної admin, використовуючи функцію alert (яка повинна показати “Іван”).
+
+let admin, name1; //used name1 instead name because nema was already in use
 
 name1 = 'Іван';
 

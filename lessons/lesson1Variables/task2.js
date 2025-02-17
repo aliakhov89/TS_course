72df97a9a0fc49dc4c97533d840f2d77889f1219
aliakhov89 <andrii.liakhov.0201@gmail.com>
2025-02-17 19:02:39 +0200

    Added data types, splitted variables from one row to different

--- lessons/lesson1Variables/task2.js
+++ lessons/lesson1Variables/task2.js
@@ -1,5 +1,5 @@
 "use strict";
 //Створіть змінну із назвою нашої планети. Як би ви назвали таку змінну?
 //Створіть змінну для зберігання імені поточного відвідувача сайту. Як би ви назвали таку змінну?
-let plnetName = 'Earth';
-let currentPlanetVisitorName = 'Andrii';
+let planetName = 'Earth';
+let currentSiteVisitorName = 'Andrii';
